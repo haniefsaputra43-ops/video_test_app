e7cc6b607b5ecd5c41beb9deda2ac06a23fe1643
unknown <haniefsaputra43-ops>
2026-05-13 17:56:41 +0700

    update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -11,12 +11,21 @@ class VideoController extends Controller
 {
     // Halaman Dashboard Customer
     public function index() {
+        $user = Auth::user();
         $videos = Video::all();
+        // dd($videos);
         return view('dashboard', compact('videos'));
     }
 
     // Fungsi minta izin nonton
     public function requestAccess($id) {
+        $permissions = Permission::where('user_id', Auth::id())->where('video_id', $id)->first();
+        if($permissions && $permissions->status === 'pending') {
+            return back()->with('error', 'Permintaan akses untuk video ini sedang diproses!');
+        }
+        if ($permissions->expires_at > now()) {
+            return back()->with('success', 'Anda masih memiliki akses untuk video ini!');
+        }
         Permission::create([
             'user_id' => Auth::id(),
             'video_id' => $id,
